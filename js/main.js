3efa76bd3e9660ce46b45d7690fa5fcf830f9a91
cochy98 <cosmoferrigno@gmail.com>
2022-03-18 15:23:04 +0100

    add function 'addNewTodo' and 'removeTodo'

--- js/main.js
+++ js/main.js
@@ -2,15 +2,27 @@ const app = new Vue(
     {
         el: '#app',
         data: {
+            inputTodoText: '',
             todoList: [
-                {
-                    text: 'Questo è il primo elemento della todo',
-                    status: false
-                },
+                'Questa è la struttura della TODO',
+                'Questa è la struttura della TODO',
+                'Questa è la struttura della TODO',
+                'Questa è la struttura della TODO',
+                'Questa è la struttura della TODO',
+                'Questa è la struttura della TODO',
+                'Questa è la struttura della TODO',
             ]
         },
         methods: {
-            
+            addNewTodo(element){
+                this.todoList.push(element);
+                console.log('aggiunto ' + element);
+                this.inputTodoText = '';
+            },
+            removeTodo(index){
+                this.todoList.splice(index, 1)
+                console.log(`rimuovo elemento all'indice ${index}`)
+            }
         }
     }
 );
