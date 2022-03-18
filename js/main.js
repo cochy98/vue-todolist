6353b43833f3e232a0557ad6273a08d2d7e0b5b7
cochy98 <cosmoferrigno@gmail.com>
2022-03-18 14:20:13 +0100

    start template TODO

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            todoList: [
+                {
+                    text: 'Questo è il primo elemento della todo',
+                    status: false
+                },
+            ]
+        },
+        methods: {
+            
+        }
+    }
+);
